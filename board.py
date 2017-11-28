44e80d0cd54e04a33f62957dfc23af4459370bdc
DIR\OKiaer <okiaer@slb.com>
2017-11-28 14:32:04 +0100

    Added iteration over days

--- board.py
+++ board.py
@@ -7,6 +7,13 @@ Created on Tue Nov 28 14:12:24 2017
 
 speeds = input("Input the speed of the boxes separated by comma:")
 speeds = [int(sp) for sp in speeds.split(',')]
+n_procs = len(speeds)
 
-throughput = "The throughput is "+str(min(speeds))
-print(throughput)
+day = 0
+while True:
+    print('\nDay '+str(day)+':')
+    throughput = 0 if day < n_procs else min(speeds)
+    print('The throughput is '+str(throughput))
+    day += 1
+    inp = input('Press enter for next day or "q" to quit')
+    if inp == 'q': break
