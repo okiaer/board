995d755ce23a075fd7bc7d6b68a2e5f0791a123b
DIR\OKiaer <okiaer@slb.com>
2017-11-28 16:02:25 +0100

    Added queues to output

--- board.py
+++ board.py
@@ -6,14 +6,33 @@ Created on Tue Nov 28 14:12:24 2017
 """
 
 speeds = input("Input the speed of the boxes separated by comma:")
+
 speeds = [int(sp) for sp in speeds.split(',')]
 n_procs = len(speeds)
-
-day = 0
+queues = [0]*n_procs
+local_throughput = [0]*n_procs
+local_throughput[0] = speeds[0]
+day = 1
 while True:
     print('\nDay '+str(day)+':')
-    throughput = 0 if day < n_procs else min(speeds)
-    print('The throughput is '+str(throughput))
+    
+    # Fill queues
+    for i in range(n_procs):
+        queues[i] = queues[i] + local_throughput[i]
+        if i > 0:
+            local_throughput[i] = 0
+    
+    # Clear queues
+    for i in range(n_procs):
+        if i > 0:
+            local_throughput[i] = min(queues[i-1], speeds[i])
+            queues[i-1] = queues[i-1] - local_throughput[i]                    
+    
+    throughput = queues[-1]
+    queues[-1] = 0
+          
+    print('Throughput: '+str(throughput))
+    print('Queues: ' + str(queues[:-1]))
     day += 1
     inp = input('Press enter for next day or "q" to quit')
     if inp == 'q': break
